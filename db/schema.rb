7c189eb9e89fce7b2d9267a736c867b6ab5c99b0
Ray Amable <rayamable@gmail.com>
2020-04-22 22:16:21 +0000

    generate scaffold invoice

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_22_220035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invoices", force: :cascade do |t|
+    t.datetime "date"
+    t.string "company"
+    t.decimal "tax"
+    t.string "salesperson"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
