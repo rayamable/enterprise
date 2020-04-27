8a924b3f9da752b3c42de2b4e77dbc34e8f83777
Ray Amable <rayamable@gmail.com>
2020-04-27 04:43:55 +0000

    added csv uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_112639) do
+ActiveRecord::Schema.define(version: 2020_04_26_005020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
